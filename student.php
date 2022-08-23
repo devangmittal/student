b537f3f4acd637051aaa334464f339544301e69b
devangmittal <mittaldevang@gmail.com>
2022-08-23 22:23:58 +0530

    Add plugin header commit
    Add namespace for plugin

---
+++ student.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Student
+ * Plugin URI: https://github.com/devangmittal/student
+ * Description: This is just a plugin build for learning WordPress.
+ * Author: Devang Mittal
+ * Version: 1.0.0
+ * Author URI: http://eaxmple.com/
+ * Text Domain: student
+ * Domain Path: /languages
+
+ * @package category
+ */
+
+namespace Devang\Student;
+
