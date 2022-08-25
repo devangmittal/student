d3d192ea5518e8fe58a2f618dab64d6986edfc4c
devangmittal <mittaldevang@gmail.com>
2022-08-25 17:23:27 +0530

    Add sub menu pending students to list all users whose status is pending

--- admin-setting.php
+++ admin-setting.php
@@ -14,18 +14,27 @@ add_action( 'admin_menu', __NAMESPACE__ . '\student_setting_menu' );
  * @return void
  */
 function student_setting_menu() {
-
+	// Student custom admin menu.
 	add_menu_page(
 		'Students',
 		'Students',
 		'manage_options',
-		'student_status',
+		'all_students',
 		__NAMESPACE__ . '\student_menu_callback_function',
 		'dashicons-media-spreadsheet'
 	);
+	// Sub menu Pending Students of Students custom menu.
+	add_submenu_page(
+		'all_students',
+		'Pending Students',
+		'Pending Students',
+		'manage_options',
+		'pending_students',
+		__NAMESPACE__ . '\pending_students_menu_callback_function',
+	);
 }
 /**
- * Student menu callback function.
+ * Create Student menu callback function.
  *
  * @return void
  */
@@ -33,6 +42,58 @@ function student_menu_callback_function() {
 	$userids = filter_input( INPUT_POST, 'student_status', FILTER_VALIDATE_INT, FILTER_REQUIRE_ARRAY );
 
 	if ( wp_verify_nonce( filter_input( INPUT_POST, '_wpnonce_update-student-status' ), 'update' ) && null !== $userids ) {
+		if ( array_key_exists( 'approve', $_POST ) ) {
+			foreach ( $userids as $userid ) {
+				update_user_meta( $userid, 'user_status', 'approve' );
+			}
+		} elseif ( array_key_exists( 'deny', $_POST ) ) {
+			foreach ( $userids as $userid ) {
+				update_user_meta( $userid, 'user_status', 'pending' );
+			}
+		}
+	}
+	$users = get_users(
+		array(
+			'role'       => 'student',
+		)
+	);
+	if ( empty( $users ) ) {
+		?>
+		<div>
+			<p>
+				No Pending students.
+			</p>
+		</div>
+		<?php
+	} else {
+		?>
+		<div>
+			<form action="<?php filter_input( INPUT_SERVER, 'REQUEST_URI' ); ?>" method="post">
+				<div>
+					<label>Pending Students: </label> <br> 
+					<?php foreach ( $users as $user ) { ?>
+						<input type="checkbox" name="student_status[]" value="<?php echo esc_html( $user->ID ); ?>"><?php echo esc_html( get_user_meta( $user->ID, 'nickname', true ) ); ?><br>
+						<?php
+					}
+					?>
+					<button type="submit" name="approve">Approve</button>
+					<button type="submit" name="deny">deny</button><?php wp_nonce_field( 'update', '_wpnonce_update-student-status' ); ?>
+				</div>
+			</form>
+		</div>
+		<?php
+	}
+}
+
+/**
+ * Create Pending Students sub menu of Students admin menu.
+ *
+ * @return void
+ */
+function pending_students_menu_callback_function() {
+	$userids = filter_input( INPUT_POST, 'student_status', FILTER_VALIDATE_INT, FILTER_REQUIRE_ARRAY );
+
+	if ( wp_verify_nonce( filter_input( INPUT_POST, '_wpnonce_update-pending-student-status' ), 'update' ) && null !== $userids ) {
 		if ( array_key_exists( 'approve', $_POST ) ) {
 			foreach ( $userids as $userid ) {
 				update_user_meta( $userid, 'user_status', 'approve' );
@@ -70,7 +131,7 @@ function student_menu_callback_function() {
 					}
 					?>
 					<button type="submit" name="approve">Approve</button>
-					<button type="submit" name="deny">deny</button><?php wp_nonce_field( 'update', '_wpnonce_update-student-status' ); ?>
+					<button type="submit" name="deny">deny</button><?php wp_nonce_field( 'update', '_wpnonce_update-pending-student-status' ); ?>
 				</div>
 			</form>
 		</div>
