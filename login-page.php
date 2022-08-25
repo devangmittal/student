98e3f77d9338c56568f565d4b5f7189184cc3dc3
devangmittal <mittaldevang@gmail.com>
2022-08-25 15:50:04 +0530

    Update update_user function to disable username email fields, delete password field update redirect_to_student_logged_in function name to log_in_student function add wp_login_form if user is logged out

--- login-page.php
+++ login-page.php
@@ -13,16 +13,8 @@ namespace Devang\login_page;
  *
  * @return void
  */
-function redirect_to_student_logged_in() {
-	?> <div> 
-	<?php
+function log_in_student() {
 	if ( is_user_logged_in() ) {
-		print_r( get_userdata( get_current_user_id(), '', true ) );
-		// user_login
-		// user_email
-		// user_url
-		// first_name
-		// last_name.
 		$userdata = get_userdata( get_current_user_id(), '', true );
 		?>
 		<div>
@@ -30,15 +22,11 @@ function redirect_to_student_logged_in() {
 				<label>User Details</label><br>
 				<div>
 					<label for="username">Username </label>
-					<input type="text" name="username" value="<?php echo esc_html( $userdata->user_login ); ?>">
-				</div>
-				<div>
-					<label for="password">Update Password </label>
-					<input type="password" name="password" value="">
+					<input type="text" name="username" value="<?php echo esc_html( $userdata->user_login ); ?>" disabled>
 				</div>
 				<div>
 					<label for="email">Email </label>
-					<input type="email" name="email" value="<?php echo esc_html( $userdata->user_email ); ?>">
+					<input type="email" name="email" value="<?php echo esc_html( $userdata->user_email ); ?>"disabled>
 				</div>
 				<div>
 					<label  for="url">Blog url </label>
@@ -52,15 +40,16 @@ function redirect_to_student_logged_in() {
 					<label for="lastname">Last Name </label>
 					<input type="text" name="lname" value="<?php echo esc_html( $userdata->last_name ); ?>">
 				</div>
-				<input type="submit" name="submit" value="Update Details"/><?php wp_nonce_field( 'update', '_wpnonce_update-details' ); ?>
+				<input type="submit" name="update" value="Update Details"/><?php wp_nonce_field( 'update', '_wpnonce_update-details' ); ?>
 
 			</form>
 		</div>
 		<?php
 	} else {
-		echo 'hello';
+		wp_login_form();
 	}
 }
+
 /**
  * Validates input fields.
  *
@@ -68,31 +57,18 @@ function redirect_to_student_logged_in() {
  */
 function validate_updation() {
 	$errors = new \WP_Error();
-	if ( empty( filter_input( INPUT_POST, 'username' ) ) || empty( filter_input( INPUT_POST, 'password' ) ) || empty( filter_input( INPUT_POST, 'email', FILTER_VALIDATE_EMAIL ) ) || empty( filter_input( INPUT_POST, 'fname' ) ) || empty( filter_input( INPUT_POST, 'lname' ) ) ) {
-		$errors->add( 'field', __( 'Required form field is missing', 'sturent' ) );
-	} elseif ( 4 > strlen( filter_input( INPUT_POST, 'username' ) ) ) {
-		$errors->add( 'username_length', __( 'Username too short. At least 4 characters is required', 'sturent' ) );
-	} elseif ( username_exists( filter_input( INPUT_POST, 'username' ) ) ) {
-		$errors->add( 'user_name', __( 'Sorry, that username already exists!', 'sturent' ) );
-	} elseif ( ! validate_username( filter_input( INPUT_POST, 'username' ) ) ) {
-		$errors->add( 'username_invalid', __( 'Sorry, the username you entered is not valid', 'sturent' ) );
-	} elseif ( 5 > strlen( filter_input( INPUT_POST, 'password' ) ) ) {
-		$errors->add( 'password', __( 'Password length must be greater than 5', 'sturent' ) );
-	} elseif ( ! is_email( filter_input( INPUT_POST, 'email', FILTER_VALIDATE_EMAIL ) ) ) {
-		$errors->add( 'email_invalid', __( 'Email is not valid', 'sturent' ) );
-	} elseif ( email_exists( filter_input( INPUT_POST, 'email', FILTER_VALIDATE_EMAIL ) ) ) {
-		$errors->add( 'email', __( 'Email Already in use', 'sturent' ) );
-	}
-	if ( ! empty( filter_input( INPUT_POST, 'url', FILTER_VALIDATE_URL ) ) ) {
+	if ( empty( filter_input( INPUT_POST, 'fname' ) ) || empty( filter_input( INPUT_POST, 'lname' ) ) ) {
+		$errors->add( 'field', __( 'Required form field is missing', 'student' ) );
+	} elseif ( ! empty( filter_input( INPUT_POST, 'url', FILTER_VALIDATE_URL ) ) ) {
 		if ( wp_http_validate_url( filter_input( INPUT_POST, 'url', FILTER_VALIDATE_URL ) ) ) {
 			return;
 		} else {
 			$errors->add( 'url', 'It is not a valid URL' );
 		}
 	} elseif ( ! validate_username( filter_input( INPUT_POST, 'fname' ) ) ) {
-		$errors->add( 'username_invalid', __( 'Sorry, Invalid First Name', 'sturent' ) );
+		$errors->add( 'username_invalid', __( 'Sorry, Invalid First Name', 'student' ) );
 	} elseif ( ! validate_username( filter_input( INPUT_POST, 'lname' ) ) ) {
-		$errors->add( 'username_invalid', __( 'Sorry, Invalid Last Name', 'sturent' ) );
+		$errors->add( 'username_invalid', __( 'Sorry, Invalid Last Name', 'student' ) );
 	}
 	if ( is_wp_error( $errors ) ) {
 
@@ -114,25 +90,12 @@ function update_user() {
 	$errors = validate_updation();
 	if ( 1 > count( $errors->get_error_messages() ) ) {
 		$updated_user_data = array(
-			'user_login' => sanitize_user( filter_input( INPUT_POST, 'username' ) ),
-			'user_email' => sanitize_email( filter_input( INPUT_POST, 'email' ) ),
-			'user_pass'  => esc_attr( filter_input( INPUT_POST, 'password' ) ),
 			'user_url'   => esc_url( filter_input( INPUT_POST, 'url' ) ),
 			'first_name' => sanitize_text_field( filter_input( INPUT_POST, 'fname' ) ),
 			'last_name'  => sanitize_text_field( filter_input( INPUT_POST, 'lname' ) ),
 		);
 		if ( wp_verify_nonce( filter_input( INPUT_POST, '_wpnonce_update-details' ), 'update' ) ) {
-			if ( ! is_wp_error( wp_update_user( $updated_user_data ) ) ) {
 				wp_update_user( $updated_user_data );
-			} else {
-				?>
-				<div>
-					<p>
-						Cannot Update User Data, Try again later.
-					</p>
-				</div>
-				<?php
-			}
 		}
 	}
 }
@@ -143,9 +106,9 @@ function update_user() {
  * @return void
  */
 function student_login_form_callable() {
-	if ( filter_input( INPUT_POST, 'submit' ) ) {
+	if ( filter_input( INPUT_POST, 'update' ) ) {
 		update_user();
 	}
-	redirect_to_student_logged_in();
+	log_in_student();
 }
 add_shortcode( 'student_login', __NAMESPACE__ . '\student_login_form_callable' );
