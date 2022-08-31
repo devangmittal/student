c9b74d421a2f2cad007be096a0ee141950343c7c
devangmittal <mittaldevang@gmail.com>
2022-08-31 13:17:53 +0530

    Enqueue custom ajax script file

--- student.php
+++ student.php
@@ -23,4 +23,21 @@ require_once MY_PLUGIN_PAT . 'admin-setting.php';
 require_once MY_PLUGIN_PAT . 'login-page.php';
 
 add_role( 'student', 'Student' );
+/**
+ * Enqueue custom scripts.
+ *
+ * @return void
+ */
+function load_scripts() {
+	wp_enqueue_script( 'ajax_update_student_meta', plugin_dir_url( __FILE__ ) . 'scripts/login-page.js', array( 'jquery' ), '1.0', true );
+	wp_localize_script(
+		'ajax_update_student_meta',
+		'update_student_ajax',
+		array(
+			'ajax_url'   => admin_url( 'admin-ajax.php' ),
+			'ajax_nonce' => wp_create_nonce( 'student_ajax_nonce' ),
+		)
+	);
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\load_scripts' );
 
