2be6ead466f99804d946192c1072568bd7ace080
devangmittal <mittaldevang@gmail.com>
2022-08-31 13:17:03 +0530

    Add scripts file login-page.js for ajax call

---
+++ scripts/login-page.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function ()
+{
+	jQuery("#update_student_meta").submit(function (e)
+	{
+		e.preventDefault();
+		var blog_url = jQuery(this).find('input[name="url"]').val();
+		var first_name = jQuery(this).find('input[name="fname"]').val();
+		var last_name = jQuery(this).find('input[name="lname"]').val();
+		var nonce = jQuery(this).find('input[name="_wpnonce_update_details"]').val();
+		console.log(blog_url + first_name + last_name + nonce);
+		jQuery.ajax(
+			{
+				url: update_student_ajax.ajax_url,
+				type: 'POST',
+				data: {
+					action: 'update_user',
+					url: blog_url,
+					fname: first_name,
+					lname: last_name,
+					ajax_nonce: update_student_ajax.ajax_nonce,
+
+				},
+				success: function (response)
+				{
+					console.log("success");
+				},
+				complete: function ()
+				{
+					console.log("Field Updated");
+				}
+			});
+	});
+});
